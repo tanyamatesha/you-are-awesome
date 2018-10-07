861d631db826487487df73cf92735f5a0514907e
Tanya <tanya-matesha@rambler.ru>
2018-10-07 20:58:45 +0300

    Task is almost completed

--- src/index.js
+++ src/index.js
@@ -19,8 +19,9 @@ const createIncrementer = function* () {
 };
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {
-};
+const returnBackInSecond = (param) => new Promise(resolve => {
+    setTimeout(() => resolve(param), 1400)
+});
 const getDeepPropertiesCount = () => {
 };
 const createSerializedObject = () => {
