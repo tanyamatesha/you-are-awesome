1022b7f1c33ef1c0ee9ef3848d4b6a209e9736f7
Tanya <tanya-matesha@rambler.ru>
2018-10-07 20:55:55 +0300

    Task is almost completed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = () => {
+    {
+    }
+};
+const createNotEnumerableProperty = () => {
+};
+const createProtoMagicObject = () => {
+};
+const incrementor = () => {
+};
+const asyncIncrementor = () => {
+};
+const createIncrementer = function* () {
+    let index = 0;
+    while (true)
+        yield ++index;
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = () => {
+};
+const getDeepPropertiesCount = () => {
+};
+const createSerializedObject = () => {
+    return null
+};
+const toBuffer = () => {
+};
+const sortByProto = (array) => {
+    array.sort((a, b) => a.__proto__ - b.__proto__);
+    return array;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
